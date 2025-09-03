303d25b2bd75fab7a450d91490d0f3a40f31aaf5
Mindi Rebeka <168852888+Mindi-R@users.noreply.github.com>
2025-09-03 16:15:41 +0530

    Create Bull Queue

---
+++ src/queues/notificationProcessor.ts
@@ -0,0 +1,14 @@
+import { notificationQueue } from "./notificationQueue";
+import { sendEmail } from "../services/sendGridService";
+
+notificationQueue.process(async(job) => {
+    if(job.data.type === "EMAIL") {
+        await sendEmail({
+            recipient: job.data.recipient,
+            subject: job.data.subject || "",
+            content: job.data.content || "",
+            textContent: job.data.content || "",
+            metadata: job.data.variables ?? {},
+        });
+    }
+})
